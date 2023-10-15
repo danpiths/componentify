3a9db97c15eabe7bef02a28f8812d348ec08c9ad
danpiths <danpiths@gmail.com>
2023-10-15 18:45:41 +0530

    unescaped entity correction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         </div>
         <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
           Chat with your <span className="text-blue-600">project reports</span>{" "}
-          like they're alive.
+          like they&apos;re alive.
         </h1>
         <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
           Componentify allows you to have conversations with any project report.
